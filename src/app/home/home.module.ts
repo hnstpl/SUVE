b0c776df9c4bc940d1d62a8e89d1b9ffcc769be1
Irulandi <irulandi@hnsonline.com>
2020-02-23 14:56:57 +0530

    Home News feed update

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "@ionic/angular";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { HomePage } from './home.page';
-
+import { HomePage } from "./home.page";
+import { HeaderComponent } from "./../header/header.component";
 @NgModule({
   imports: [
     CommonModule,
@@ -13,11 +13,11 @@ import { HomePage } from './home.page';
     IonicModule,
     RouterModule.forChild([
       {
-        path: '',
+        path: "",
         component: HomePage
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, HeaderComponent]
 })
 export class HomePageModule {}
